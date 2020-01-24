ebea25ab24ed3eec0c5aae52315ff4656547d9bb
zhaotbj <975619483@qq.com>
2020-01-24 21:47:36 +0800

    'fixed'

--- database/schema/Article.js
+++ database/schema/Article.js
@@ -11,8 +11,8 @@ const articleSchema = new Schema({
   readNumber: Number, // 章阅读量
   commentNumber: Number, // 文章评论数
   thumbUpNumber: Number, // 文章点赞数
-  createTime: Number, //  创建时间
-  modifiedTime: Number, //修改时间
+  createTime: String, //  创建时间
+  modifiedTime: String, //修改时间
 }, {
   collections: 'Article'
 })
